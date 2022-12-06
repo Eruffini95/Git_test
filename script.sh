693fb4934c8f8e72c312eae0343d0a8ad365d9bd
unknown <eruffini@myges.fr>
2022-12-06 15:49:22 +0100

    script user plus ssh

--- script.sh
+++ script.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+# Copier le fichier de configuration sur le serveur
+
+cp ./config__files/ssh/sshd_config /etc/ssh/sshd_config
+
+cp ./config__files/ssh/Banner /etc/Banner
+
+
+# Redemarrer le service sshd
+
+systemctl restart sshd
+
 USER="usertest1"
 
 PASSWORD="Respons11"
